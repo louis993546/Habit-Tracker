c8cf338408ab475e9ba4d7e8fd6d885aa776b61e
Louis Tsai <louis993546@gmail.com>
2021-02-07 13:58:23 +0100

    Initial Commit

---
+++ Shared/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Shared
+//
+//  Created by Louis Tsai on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
